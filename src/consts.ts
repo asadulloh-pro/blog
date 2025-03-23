e12cf68c4406b2d186227897ed69667ede28d987
Asadulloh-Open <asadullosadirdinov@gmail.com>
2025-03-24 03:44:27 +0500

    feat: starter

---
+++ src/consts.ts
@@ -0,0 +1,39 @@
+import type { Metadata, Site, Socials } from "@types";
+
+export const SITE: Site = {
+  TITLE: "console.log",
+  DESCRIPTION: "Insights, experiences, and lessons in software engineering.",
+  EMAIL: "asadullosadirdinov@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "Exploring the world of code, one log at a time.",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "Thoughts, experiences, and deep dives into software engineering.",
+};
+
+export const PROJECTS: Metadata = {
+  TITLE: "Projects",
+  DESCRIPTION: "A showcase of my work, from ideas to real-world applications.",
+};
+
+export const SOCIALS: Socials = [
+  {
+    NAME: "Linkedin",
+    HREF: "https://www.linkedin.com/in/asadulloh-sadirdinov-8252a3213/",
+  },
+  {
+    NAME: "GitHub",
+    HREF: "https://github.com/asadulloh-pro",
+  },
+  {
+    NAME: "Website",
+    HREF: "https://asadulloh.com",
+  },
+];
